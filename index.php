0927f6ff4037781bac96f9ea2b8cb69fb80dbe81
Diosmar Cardenas <diosmararbeycardenasduran@MacBook-Pro-de-Diosmar.local>
2023-09-05 17:08:33 -0500

    Other basic operations are added

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
     //object creation, "addition".
     $Operation_1 = new Operation(20, 6);
     $totalAdd = $Operation_1->getAdd();
-    echo $totalAdd;
+    echo "Total Addition: " . $totalAdd . "<br>";
+
+    $Operation_1 = new Operation(20, 6);
+    $totalAdd = $Operation_1->getSubtract();
+    echo "Total Sustraction: " . $totalAdd . "<br>";
+
+    $Operation_1 = new Operation(20, 6);
+    $totalAdd = $Operation_1->getProduct();
+    echo "Total Product: " . $totalAdd . "<br>";
+
+    $Operation_1 = new Operation(20, 6);
+    $totalAdd = $Operation_1->getDivision();
+    echo "Total Division: " . $totalAdd . "<br>";
 
 ?>
